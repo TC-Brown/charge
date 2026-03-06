45b712ddf291c2a5e30d553a07bdc70bc8c28277
TC Brown <trrncbrown@gmail.com>
2026-03-06 04:38:39 -0600

    [v0.0.0-beta]

--- charge.js
+++ charge.js
@@ -3,3 +3,7 @@ const gitLink = document.getElementById('githubBtn');
 gitLink.addEventListener('click', () => { 
     window.location.href = "https://github.com/tc-brown/";
 });
+
+function openApp() {
+    window.location.href = "https://tc-brown.github.io/thunderbolt/";
+}
