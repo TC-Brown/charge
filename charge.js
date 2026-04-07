cabad7953bc111f95a7e3e58094c1e351a5dfc61
TC Brown <trrncbrown@gmail.com>
2026-04-07 15:37:03 -0500

    [TEST]

--- charge.js
+++ charge.js
@@ -2,19 +2,19 @@
 const repoBtn = document.getElementById('repos');
 repoBtn.addEventListener('click', () => {
     window.location.href = 'https://github.com/TC-Brown';
-})
+});
 
 // Point-of-Sale Button
 const posBtn = document.getElementById('pos');
 posBtn.addEventListener('click', () => {
     window.location.href = 'apps/pos/index.html';
-})
+});
 
 // Invenetory Button
 const invBtn = document.getElementById('inventory');
 invBtn.addEventListener('click', () => {
     window.location.href = 'apps/inventory/index.html';
-})
+});
 
 // Prep Button
 const prepBtn = document.getElementById('prep');
